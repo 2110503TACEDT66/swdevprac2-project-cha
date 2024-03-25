0f503d21f1f6d080a4d164e4e541aa34e8837b73
Napak Jessdaapiban <darkbeatmoss@gmail.com>
2024-03-26 00:20:25 +0700

    Add interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,11 +1,50 @@
 export interface ReservationItem {
-    carId: string,
+    carId : string,
     carModel: string,
     numOfDays: number,
     pickupDate: string,
     pickupLocation: string,
     returnDate: string,
     returnLocation: string
+}
+
+export interface BookingItem {
+    _id: string,
+    pickupDate: string,
+    pickupLocation: string,
+    returnDate: string,
+    returnLocation: string,
+    user: string,
+    car: {
+        _id: string,
+        model: string,
+        description: string,
+        picture: string,
+        id: string
+    },
+    createdAt: string,
+    __v: number
+}
+
+export interface BookingJson {
+    success: boolean,
+    count: number,
+    data: BookingItem[]
+}
+
+export interface BookingBody {
+    pickupDate: string,
+    pickupLocation: string,
+    returnDate: string,
+    returnLocation: string,
+}
+
+export interface RegisterItem {
+    name: string,
+    email: string,
+    tel: string,
+    password: string,
+    role: string
 }
 
 export interface CarItem {
